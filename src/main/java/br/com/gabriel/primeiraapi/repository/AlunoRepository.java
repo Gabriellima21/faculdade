7be8e695ff5db085e224d77b5f57a9c4bdfb51c3
Gabriel Donizete dos Santos Lima <gabrieldonizete0@gmail.com>
2023-12-22 02:04:42 -0300

    Interface AlunoRepository
    Signed-off-by: Gabriel Donizete dos Santos Lima <gabrieldonizete0@gmail.com>

--- src/main/java/br/com/gabriel/primeiraapi/repository/AlunoRepository.java
+++ src/main/java/br/com/gabriel/primeiraapi/repository/AlunoRepository.java
@@ -1,9 +1,8 @@
 package br.com.gabriel.primeiraapi.repository;
 
-import java.util.List; 
+import java.util.List;  
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.gabriel.primeiraapi.entity.Aluno;
